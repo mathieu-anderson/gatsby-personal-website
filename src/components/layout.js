11d23621befa0ac34ca4928abafd009b27759c38
Mathieu Anderson <anderson@talon.one>
2020-04-04 10:43:37 +0200

    Remove default header things

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,40 +7,27 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 1200,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main className="layout-grid">{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 1200,
+        padding: `0 1.0875rem 1.45rem`,
+      }}
+    >
+      <Header />
+      <main className="layout-grid">{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
+    </div>
   )
 }
 
