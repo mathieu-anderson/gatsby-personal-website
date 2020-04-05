64ab5d83767dc147e764430b933e4eefe138262a
Mathieu Anderson <anderson@talon.one>
2020-04-05 12:41:42 +0200

    Remove footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,25 +8,20 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import Header from "./header"
+import Header from "./Header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
   return (
     <div
       style={{
-        margin: `0 auto`,
+        margin: `1rem auto`,
         maxWidth: 1200,
         padding: `0 1.0875rem 1.45rem`,
       }}
     >
       <Header />
       <main className="layout-grid">{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </footer>
     </div>
   )
 }
