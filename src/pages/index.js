29b5237988c76d115dea57ece5995e600266f5ed
Mathieu Anderson <anderson@talon.one>
2020-04-05 16:05:25 +0200

    Add content for What I do, add font-sizes for p

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,16 +7,30 @@ import Box from "../components/Box"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Box className="Section-1" heading="The box 1 is here">
+    <Box className="Section-1" heading="What I do">
+      <p>
+        I am a <b>front-end</b> specialist. I love being part of
+        cross-functional teams and being the <b>logical glue</b>
+        between the back-end devs and the designers.
+      </p>
+      <p>
+        My favourite tool is currently <b>React</b>, but I love anything that
+        allows to build UIs in a <b>declarative way</b>. And I'm always eager to
+        adopt whatever is consensual in the team!
+      </p>
+      <p>
+        I come from a <b>fullstack background</b>. I am familiar with back-end
+        tech (Node, Go, SQL databases), and I'm delighted to dive into it when
+        it is required!
+      </p>{" "}
+    </Box>
+    <Box className="Section-2" heading="How I get it done">
       Indeed it is
     </Box>
-    <Box className="Section-2" heading="The box 2 is here">
+    <Box className="Section-3" heading="Where I did it">
       Indeed it is
     </Box>
-    <Box className="Section-3" heading="The box 3 is here">
-      Indeed it is
-    </Box>
-    <Box className="Section-4" heading="The box 4 is here">
+    <Box className="Section-4" heading="On the side">
       Indeed it is
     </Box>
   </Layout>
