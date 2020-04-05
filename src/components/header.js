e166230353d58515449fb25f14716deb372a0dd1
Mathieu Anderson <anderson@talon.one>
2020-04-05 12:43:56 +0200

    Add social icons to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,17 @@
 import React from "react"
+import { GithubIcon, MediumIcon, TwitterIcon, LinkedinIcon } from "./Icons"
+
+import "./Header.css"
 
 const Header = () => (
-  <header>
+  <header className="header-parent">
     <h1>Mathieu Anderson is a web dev</h1>
+    <div className="header-social">
+      <GithubIcon />
+      <MediumIcon />
+      <TwitterIcon />
+      <LinkedinIcon />
+    </div>
   </header>
 )
 
