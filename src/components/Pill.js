66e5d22229d1a79a010b684f20c8ad9590d8056f
Mathieu Anderson <anderson@talon.one>
2020-04-08 18:39:14 +0200

    Add content of Pills, add animation

--- src/components/Pill.js
+++ src/components/Pill.js
@@ -1,11 +1,38 @@
 import React from "react"
 import "./Pill.css"
 
-const Pill = ({ icon, name }) => {
+const Pill = ({ icon, name, details }) => {
+  const [showDetails, setShowDetails] = React.useState(false)
   return (
-    <button className={`pill ${name.toLowerCase()}-pill`}>
-      {icon}
-      <div className="pill-name">{name}</div>
+    <button
+      className={`${
+        showDetails ? "open" : "close"
+      } pill ${name.toLowerCase()}-pill`}
+      onClick={() => {
+        setShowDetails(!showDetails)
+      }}
+    >
+      <div className="pill-head">
+        {icon}
+        <div className="pill-name">{name}</div>
+      </div>
+      <div
+        className="pill-details"
+        aria-hidden={`${showDetails ? "false" : "true"}`}
+      >
+        <div>
+          <b>Since </b>
+          {details.since}
+        </div>
+        <div>
+          <b>Using it </b>
+          {details.frequency}
+        </div>
+        <div>
+          <b>Love </b>
+          {details.like}
+        </div>
+      </div>
     </button>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -40,17 +40,107 @@ const IndexPage = () => (
     </Box>
     <Box className="Section-2" heading="How I get it done">
       <div className="pills-container">
-        <Pill name="JavaScript" icon={<JavaScriptIcon />} />
-        <Pill name="TypeScript" icon={<TypeScriptIcon />} />
-        <Pill name="React" icon={<ReactIcon />} />
-        <Pill name="Redux" icon={<ReduxIcon />} />
-        <Pill name="CSS" icon={<CssIcon />} />
-        <Pill name="Sass" icon={<SassIcon />} />
-        <Pill name="Node" icon={<NodeIcon />} />
-        <Pill name="Webpack" icon={<WebpackIcon />} />
-        <Pill name="Docker" icon={<DockerIcon />} />
-        <Pill name="Git" icon={<GitIcon />} />
-        <Pill name="CircleCi" icon={<CircleCiIcon />} />
+        <Pill
+          name="JavaScript"
+          icon={<JavaScriptIcon />}
+          details={{
+            since: "2016",
+            frequency: "every day",
+            like:
+              "the flexibility to be used in the front-end and the back-end, the speed and ease of development.",
+          }}
+        />
+        <Pill
+          name="TypeScript"
+          icon={<TypeScriptIcon />}
+          details={{
+            since: "2019",
+            frequency: "every day",
+            like:
+              "the safety and productivity boost it brings to large and complex projects.",
+          }}
+        />
+        <Pill
+          name="React"
+          icon={<ReactIcon />}
+          details={{
+            since: "2016",
+            frequency: "every day",
+            like:
+              "the composability paradigm and the continued improvements and innovation brought to the API (hooks <3)",
+          }}
+        />
+        <Pill
+          name="Redux"
+          icon={<ReduxIcon />}
+          details={{
+            since: "2016",
+            frequency: "every day",
+            like:
+              "the core simplicity of the design and the power it offers (to be used wisely!)",
+          }}
+        />
+        <Pill
+          name="CSS"
+          icon={<CssIcon />}
+          details={{
+            since: "2016",
+            frequency: "every day",
+            like: `the potential to create anything with enough knowledge (like ${(
+              <a href="https://youtu.be/wlfmSJIyg_E">Mario Kart</a>
+            )})`,
+          }}
+        />
+        <Pill
+          name="Sass"
+          icon={<SassIcon />}
+          details={{
+            since: "2016",
+            frequency: "only in side projects",
+            like:
+              "the flexibility to be used in the front-end and the back-end, the speed and ease of development",
+          }}
+        />
+        <Pill
+          name="Node"
+          icon={<NodeIcon />}
+          details={{
+            since: "2016",
+            frequency: "only in side projects",
+            like:
+              "the package ecosystem around NPM and the ability to write JS / TS in the back-end for truly isomorphic apps",
+          }}
+        />
+        <Pill
+          name="Webpack"
+          icon={<WebpackIcon />}
+          details={{
+            since: "2016",
+            frequency: "every day",
+            like:
+              "the amount of control it gives over a build and the excellent developer experience it enables",
+          }}
+        />
+        <Pill
+          name="Docker"
+          icon={<DockerIcon />}
+          details={{
+            since: "2017",
+            frequency: "every day (as part of the dev and CI environment)",
+            like:
+              "the ability to eliminate local setup issues by spinning everything up in containers",
+          }}
+        />
+        <Pill
+          name="Git"
+          icon={<GitIcon />}
+          details={{
+            since: "2016",
+            frequency: "every day",
+            like:
+              "the security, debugging and collaboration potential it offers.",
+          }}
+        />
       </div>
     </Box>
     <Box className="Section-3" heading="Where I did it">
